20bb199f9153ca34f3877a29aadf7c7e2eccd9e4
ershov-ilya <ershov.ilya@gmail.com>
2015-08-30 09:40:15 +0300

    submodules

---
+++ api/ajax/form/get/index.php
@@ -0,0 +1,55 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 10.08.2015
+ * Time: 11:00
+ */
+
+header("Access-Control-Allow-Origin: http://billing.hostconfig.ru");
+
+// Errors control
+header('Content-Type: text/plain; charset=utf-8');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+if(isset($_GET['t'])) define('DEBUG', true);
+defined('DEBUG') or define('DEBUG', false);
+$response=array(
+    'data' => array()
+);
+
+/* MODX
+------------------------------------------------------------------- */
+//defined('MODX_API_MODE') or define('MODX_API_MODE', true);
+//require_once('../../../../../../index.php');
+
+
+/* CONFIG
+------------------------------------------------------------------- */
+require_once('../../../core/config/api.private.config.php');
+require_once(API_CORE_PATH.'/class/restful/restful.class.php');
+require_once(API_CORE_PATH.'/class/database/database.class.php');
+require_once(API_CORE_PATH.'/config/pdo.private.config.php');
+
+/* @var modX $modx*/
+
+try {
+    $rest = new RESTful('get_form','id,idperson,date_hired,fio,idrequest,idrequest2,type,bank,numinstall,coast,coast2,status,idbpo,city');
+	
+	$db=new Database($pdoconfig);
+	$table=$db->getTableWhere('requesttable','',$rest->data);
+	
+	$response['data']=$table;
+}
+catch(Exception $e){
+    $response['message']=$e->getMessage();
+    $response['code']=$e->getCode();
+}
+
+require_once(API_CORE_PATH.'/class/format/format.class.php');
+if(DEBUG) print Format::parse($response, 'php');
+else  print Format::parse($response, 'json');
+/**/
---
+++ static/filter.js
@@ -0,0 +1,65 @@
+﻿/**
+ * Created by
+ * User: ershov-ilya
+ * Website: ershov.pw
+ * GitHub : https://github.com/ershov-ilya
+ * on 20.08.2015.
+ */
+
+var FILTER = (function(){
+    var PUBLIC={
+        options:{},
+        data:{}
+    };
+    // Private
+    var DEBUG=true;
+    var scopes=[];
+    var url;
+    var FirstData;
+
+    function filterChange(){
+        $this=$(this);
+        var name=$this.attr('name');
+        var value=$this.find(':selected').val();
+        if(DEBUG) console.log('filterChange() '+name+' '+value);
+        PUBLIC.options[name]=value;
+        if(PUBLIC.options[name]=="") delete(PUBLIC.options[name]);
+        $(document).trigger('FILTER.GetData');
+    }
+
+    function getData(){
+		if(DEBUG) console.log('FILTER.getData()');
+        $.ajax({
+            url: url,
+            data: PUBLIC.options
+        }).success(function(response){
+            response=JSON.parse(response);
+            if(typeof response.data != 'undefined') PUBLIC.data=response.data;
+            if(!FirstData){
+                FirstData=true;
+                $(document).trigger('FILTER.FirstData');
+            }
+            $(document).trigger('FILTER.LoadDone');
+        });
+    }
+
+    PUBLIC.init=function(connector_url){
+		if(DEBUG) console.log('FILTER.init()');
+        FirstData=false;
+        if(typeof connector_url != 'undefined') {
+		if(DEBUG) console.log('url='+connector_url);
+			url=connector_url;
+		}
+        else{
+            console.error('Не указан коннектор фильтра данных');
+            return false;
+        }
+
+        $('.filter-select').change(filterChange);
+        $(document).off('FILTER.GetData');
+        $(document).on('FILTER.GetData',getData);
+		$(document).trigger('FILTER.GetData');
+    };
+
+    return PUBLIC;
+})();
